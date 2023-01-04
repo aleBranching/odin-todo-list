9b6606f2b8410dffc77c6ce106d6803beb2c4150
aleTesting <aleksas773@googlemail.com>
2023-01-04 18:10:23 +0000

    remove unneded experiment files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import TaskHolder from "./modules/tasksHolder";
-import Car from "./modules/test";
 
-const main = new Car();
 // console.log(main);
 
 const trying = new TaskHolder();
-
-console.log(main);
-console.log(trying);
+trying.createTask();
+console.log(trying.tasks);
